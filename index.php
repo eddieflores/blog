7581c57ca739d127a6495dc6436370a1887d547b
eleuterio flores <floreseddie99@gmail.com>
2014-11-04 12:08:29 -0800

    no message

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
   <?php
        require_once (_DIR_ . "/view/header.php");
-       require_once(__DIR__ . "/controller/create-db.php");
+       require_once (_DIR_ . "/view/navigation.php");
+       require_once(_DIR_ . "/controller/create-db.php");
        require_once(_DIR_ . "/view/footer.php");
   ?>
   
